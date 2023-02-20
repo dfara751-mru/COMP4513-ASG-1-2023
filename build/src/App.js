4149149874c0039405625d92fa9ecc2ffcaacdea
Jordan <jwalk457@mtroyal.ca>
2023-02-20 09:48:56 -0700

    Getting background image up.

--- build/src/App.js
+++ build/src/App.js
@@ -2,19 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import * as cloneDeep from "lodash/cloneDeep";
+import theatreImage from "./theatre.jpg"
 
 function App() {
+
   return (
 
-    // style={{backgroundImage: 'url(https://unsplash.com/photos/nLl5sJnElxY)'
+    // style={{backgroundImage: 'url(https://unsplash.com/photos/nLl5sJnElxY)'}}
     <div
-      className="min-height-full min-w-full h-[100%]"
-      
+      className="bg-cover bg-center w-full h-full grid grid-col-3 grid-rows-4 justify-items-center bg-local gap-5"
+      style={{backgroundImage: `url(${theatreImage})` }}
     >
-      <div>Header</div>
-      <div>Movie Filter</div>
-      <div>List/Matches</div>
-      <div>Favorites</div>
+      {/* <div className="height-full w-full bg-red-600 ">Header</div>
+      <div className="height-full w-full bg-blue-600">Movie Filter</div>
+      <div className="height-full w-full bg-yellow-600">List/Matches</div>
+      <div className="height-full w-full bg-green-600">Favorites</div> */}
     </div>
   );
 }
