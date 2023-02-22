49373f16b04c585279c6cab5bd1b3a192b5af4d6
Jordan <jwalk457@mtroyal.ca>
2023-02-22 10:20:35 -0700

    router problem

--- build/src/App.js
+++ build/src/App.js
@@ -4,7 +4,8 @@ import * as cloneDeep from "lodash/cloneDeep";
 import theatreImage from "./theatre.jpg";
 import HeaderApp from "./components/HeaderApp.js";
 import Filter from "./components/Filter.js";
-import List from "./components/List.js"
+import List from "./components/List.js";
+import Home from "./components/Home.js";
 
 import React, { useEffect, useState } from "react";
 
@@ -13,25 +14,25 @@ import Favorites from "./components/Favorites";
 
 function App() {
   const [movies, setMovies] = React.useState([]);
-  const [favorites,setFavorites] = React.useState([]);
+  const [favorites, setFavorites] = React.useState([]);
 
-  const saveFavorites = function(movie) {
-
-    if (favorites.findIndex(currentFavMovie => currentFavMovie.id === movie.id) < 0) {
+  const saveFavorites = function (movie) {
+    if (
+      favorites.findIndex(
+        (currentFavMovie) => currentFavMovie.id === movie.id
+      ) < 0
+    ) {
       let newFavorites = [];
       newFavorites = favorites;
       const favMovie = cloneDeep(movie);
-      newFavorites.push(favMovie)
+      newFavorites.push(favMovie);
       setFavorites(newFavorites); // adds movie to favorites list.
       console.log("favorites");
       console.log(favorites);
     } else {
       console.log("The movie is already in Favorites!");
     }
-   
-  }
-  
-  
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -68,29 +69,36 @@ function App() {
   //note make sure to use cols in parent and col or row in child.
   // style={{backgroundImage: 'url(https://unsplash.com/photos/nLl5sJnElxY)'}}
   return (
-    <main
-      className="grid gap-1 grid-cols-5 grid-rows-5 bg-cover bg-center w-full h-full justify-items-center bg-local gap-5"
-      style={{ backgroundImage: `url(${theatreImage})` }}
-    >
-      <HeaderApp />
-      <Filter/>
-      <List movies={movies} saveFavorites={saveFavorites}/>
-      <Favorites favorites={favorites}/>
-      {/* <Routes>
-        <Route path="/" exact element={<Home/>} />
-      </Routes> */}
-
-      {/* <div className="w-full h-24 min-h-[3] rounded-lg bg-red-600 col-span-3">Header</div> */}
-      {/* <div className="row-span-2 w-full h-full min-w-[150px] min-h-[50px] rounded-lg bg-blue-600">
-        Movie Filter
-      </div> */}
-      {/* <div className="col-span-3 row-span-4 w-full min-w-[150px] min-h-[50px] rounded-lg bg-yellow-600">
-        List/Matches
-      </div> */}
-      {/* <div className="w-full min-w-[150px] min-h-[50px] cols-span-1 rounded-lg bg-green-600">
-        Favorites
-      </div> */}
+    <main>
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/home" exact element={<Home />} />
+    </Routes>
     </main>
+
+    // <main
+    //   className="grid gap-1 grid-cols-5 grid-rows-5 bg-cover bg-center w-full h-full justify-items-center bg-local gap-5"
+    //   style={{ backgroundImage: `url(${theatreImage})` }}
+    // >
+    //   {/* <HeaderApp />
+    //   <Filter/>
+    //   <List movies={movies} saveFavorites={saveFavorites}/>
+    //   <Favorites favorites={favorites}/> */}
+    //   <Routes>
+    //     <Route path="/" exact element={<Home/>} />
+    //   </Routes>
+
+    //   {/* <div className="w-full h-24 min-h-[3] rounded-lg bg-red-600 col-span-3">Header</div> */}
+    //   {/* <div className="row-span-2 w-full h-full min-w-[150px] min-h-[50px] rounded-lg bg-blue-600">
+    //     Movie Filter
+    //   </div> */}
+    //   {/* <div className="col-span-3 row-span-4 w-full min-w-[150px] min-h-[50px] rounded-lg bg-yellow-600">
+    //     List/Matches
+    //   </div> */}
+    //   {/* <div className="w-full min-w-[150px] min-h-[50px] cols-span-1 rounded-lg bg-green-600">
+    //     Favorites
+    //   </div> */}
+    // </main>
   );
 }
 
