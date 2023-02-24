a2f22e3ef3fc2cd29dd16cf86f85bf9b506a6ce2
dfara751-mru <dfara751@mtroyal.ca>
2023-02-24 14:48:47 -0700

    rating stars and display on details

--- build/src/components/Detail.js
+++ build/src/components/Detail.js
@@ -6,6 +6,10 @@ import HeaderApp from "./HeaderApp.js";
 import Footer from"./Footer";
 import theatreImage from "../theatre.jpg";
 
+import fullstar from "../icons/star-solid.svg"
+import halfstar from "../icons/star-half-stroke-solid.svg"
+import emptystar from "../icons/star-regular.svg"
+
 Modal.setAppElement('#root') //gets rid of an error regarding Modal
 
 const Detail = props => {
@@ -24,6 +28,14 @@ const handleFavorite = () => {
   setFav(!faved);
 };
 
+let genres = [];
+// Some movies have "null" for their genres list
+// This will replace the null list with just an empty one so the page can still display without error
+if (movie.details.genres == null){
+  genres = [];
+} else {
+  genres = movie.details.genres;
+}
 
 const checkFav = () =>{
   if (props.favorites.find(m => m.id === movie.id) != undefined){
@@ -50,8 +62,39 @@ const closeModal = () => {
 }
 
 
+// Behold, the most obtuse way of making rating stars:
+let fullstars = [];
+let halfstars = [];
+let emptystars = [];
+const ratingStars = () => {
 
+  const index = Math.round(movie.ratings.average);
 
+  console.log(movie.ratings.average)
+
+  let full = Math.round(index/2)-(index % 2);
+  let half = index % 2;
+  
+
+  for(let i = 0; i < full; i++){
+    fullstars.push("full"+i);
+  }
+
+  for(let i = 0; i < half; i++){
+    halfstars.push("half"+i);
+  }
+
+  //console.log(Math.round(5-(half+full)))
+  for(let i = 0; i < (5-(half+full)); i++){
+    emptystars.push("empty"+i);
+  }  
+}
+
+const handleRating = (id) => {
+
+}
+
+ratingStars();
 
 //const poster = `https://image.tmdb.org/t/p/w154/${this.props.currentMovie.poster}`;
 
@@ -75,12 +118,12 @@ const closeModal = () => {
             <h3 className="font-mono">{movie.tagline}</h3>
             <br/>
             <button onClick={handleFavorite} 
-            className={faved ? "bg-slate-200 rounded-lg w-40 h-10 border-2 border-solid border-black font-mono":"bg-slate-50 rounded-lg w-40 h-10 border-2 border-solid border-black font-mono"}>
+            className={faved ? "hover:opacity-50 bg-slate-200 rounded-lg w-40 h-10 border-2 border-solid border-black font-mono":"hover:opacity-50 bg-slate-50 rounded-lg w-40 h-10 border-2 border-solid border-black font-mono"}>
             {faved ? "FAVORITED" : "FAVORITE"}
             </button>
             <br/>
             <br/>
-            <Link to="/build/browse"><button className="bg-slate-50 rounded-lg w-40 h-10 border-2 border-solid border-black font-mono">BACK TO SEARCH</button></Link>
+            <Link to="/build/browse"><button className="hover:opacity-50 bg-slate-50 rounded-lg w-40 h-10 border-2 border-solid border-black font-mono">BACK TO SEARCH</button></Link>
             <br/>
             <br/>
             <p className="font-mono"><strong>Released:</strong> {movie.release_date}</p>
@@ -92,13 +135,13 @@ const closeModal = () => {
             <br />
             <p className="font-mono"><strong>Genres:</strong></p>
             <ul>
-              {movie.details.genres.map((m) => (<li className="font-mono">{m.name}</li>))}
+              {genres.map((m) => (<li className="font-mono" key={m.id}>{m.name}</li>))}
             </ul>
             <br />
             <p className="font-mono"><strong>Links:</strong></p>
-            <a href={"https://www.themoviedb.org/movie/" + movie.tmdb_id}>TMDB</a>
+            <a className="hover:opacity-50" href={"https://www.themoviedb.org/movie/" + movie.tmdb_id}>TMDB</a>
             <br/>
-            <a href={"https://www.imdb.com/title/" + movie.imdb_id}>IMDB</a>
+            <a className="hover:opacity-50" href={"https://www.imdb.com/title/" + movie.imdb_id}>IMDB</a>
             </div>
 
             <div>
@@ -109,8 +152,31 @@ const closeModal = () => {
               </img>
               <br />
               <div className="border-4 rounded-md w-full">
-                Ratings go here
-                
+                <p className="font-mono text-3xl"><strong>★RATINGS★</strong></p>
+                <br/>
+                <p className="font-mono"><strong>Average Rating:</strong></p>
+                <div className="flex justify-center">
+                {fullstars.map((star) => 
+                  (<img style={{ height: 40, width: 40}} src= {fullstar}  alt="star" key={star}></img>)
+                  )}
+                {halfstars.map((star) => 
+                  (<img style={{ height: 40, width: 40}} src= {halfstar}  alt="star" key={star}></img>)
+                )}
+                {emptystars.map((star) => 
+                  (<img style={{ height: 40, width: 40}} src= {emptystar}  alt="star" key={star}></img>)
+                )}
+
+
+                </div>
+                <p className="font-mono"><strong>Your Rating:</strong></p>
+                <div className="flex justify-center">
+                  <img className="hover:opacity-50" style={{ height: 40, width: 40}} src={emptystar}  alt="half"></img>
+                  <img className="hover:opacity-50" style={{ height: 40, width: 40}} src={emptystar}  alt="half"></img>
+                  <img className="hover:opacity-50" style={{ height: 40, width: 40}} src={emptystar}  alt="half"></img>
+                  <img className="hover:opacity-50" style={{ height: 40, width: 40}} src={emptystar}  alt="half"></img>
+                  <img className="hover:opacity-50" style={{ height: 40, width: 40}} src={emptystar}  alt="half"></img>
+                </div>
+
               </div>
               <br/>
               
