13f217bccb3a456b436f1413bb684d27ec5135f7
Jordan <jwalk457@mtroyal.ca>
2023-02-27 22:09:17 -0700

    attempt implementation of spinner

--- build/src/App.js
+++ build/src/App.js
@@ -3,17 +3,15 @@ import "./App.css";
 import * as cloneDeep from "lodash/cloneDeep";
 import Home from "./components/Home.js";
 import BrowseMovies from "./components/BrowseMovies";
-import Detail from "./components/Detail"
+import Detail from "./components/Detail";
 import React, { useEffect, useState } from "react";
 
 import { Route, Routes } from "react-router-dom";
 
-
-
 function App() {
   const [moviesData, setMoviesData] = React.useState([]);
   const [favorites, setFavorites] = React.useState([]);
-  
+  const [spinner, setSpinner] = React.useState(false);
   let ratings = []; // array for storing user ratings on movies (saves an object as {id: movie id, amount: no of stars given})
 
   const saveFavorites = function (movie) {
@@ -36,16 +34,17 @@ function App() {
         (currentFavMovie) => currentFavMovie.id === movie.id
       ) >= 0
     ) {
-      let index = favorites.findIndex((currentFavMovie) => currentFavMovie.id === movie.id);
+      let index = favorites.findIndex(
+        (currentFavMovie) => currentFavMovie.id === movie.id
+      );
       let tempFavs = cloneDeep(favorites);
       tempFavs.splice(index, 1);
       setFavorites(tempFavs); // adds movie to favorites list.
-      console.log(favorites)
+      console.log(favorites);
     } else {
       console.log("The movie was not in favorites");
     }
-  }
-
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -53,14 +52,15 @@ function App() {
         // check if movies is already full.
         if (moviesData.length <= 0) {
           if (localStorage.getItem("movies") === null) {
-            // fetch data and store in local storage.
             console.log("fetch from URL");
             const url =
               "https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?limit=200";
+            setSpinner(true);
             const response = await fetch(url);
             const data = await response.json();
             initiateTitleSort(data);
             localStorage.setItem("movies", JSON.stringify(data));
+            setSpinner(false);
           } else {
             // get data from storage.
             const rawData = localStorage.getItem("movies");
@@ -76,6 +76,7 @@ function App() {
       }
     };
 
+
     const initiateTitleSort = (movies) => {
       let newMovies = movies.sort(function (a, b) {
         if (a.title < b.title) return -1;
@@ -90,16 +91,38 @@ function App() {
     getData();
   }, []);
 
-  //note make sure to use cols in parent and col or row in child.
+  
   // style={{backgroundImage: 'url(https://unsplash.com/photos/nLl5sJnElxY)'}}
   return (
     <Routes>
-      <Route path="/build/" exact element={<BrowseMovies moviesData={moviesData} favorites={favorites} saveFavorites={saveFavorites} setFavorites={setFavorites} />} />
-      <Route path="/build/home" exact element={<Home />} />
-      <Route path="/build/detail" exact element={<Detail favorites={favorites} saveFavorites={saveFavorites} ratings={ratings} removeFavorite={removeFavorite}/>} />
-      <Route path="/build/browse" exact element={<BrowseMovies moviesData={moviesData} favorites={favorites} saveFavorites={saveFavorites} setFavorites={setFavorites} removeFavorite={removeFavorite}/>} />
+      <Route path="/build/" exact element= {<Home spinner={spinner} />} />
+      <Route path="/build/home" exact element= {<Home spinner={spinner} />} />
+      <Route
+        path="/build/detail"
+        exact
+        element={
+          <Detail
+            favorites={favorites}
+            saveFavorites={saveFavorites}
+            ratings={ratings}
+            removeFavorite={removeFavorite}
+          />
+        }
+      />
+      <Route
+        path="/build/browse"
+        exact
+        element={
+          <BrowseMovies
+            moviesData={moviesData}
+            favorites={favorites}
+            saveFavorites={saveFavorites}
+            setFavorites={setFavorites}
+            removeFavorite={removeFavorite}
+          />
+        }
+      />
     </Routes>
-
   );
 }
 
--- build/src/components/Home.js
+++ build/src/components/Home.js
@@ -11,9 +11,11 @@ const Home = (props) => {
   const handleInput = (e) => {
     setInput(e.target.value);
   }
-
-  return (
-    <main
+  const printHome = () => {
+    if(props.spinner) {
+      return <img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif" />
+    } else {
+      return <main
       className="grid grid-cols-3 grid-rows-3 bg-cover bg-center w-full h-full m-auto bg-local place-content-center"
       style={{ backgroundImage: `url(${theatreImage})` }}
     >
@@ -27,6 +29,27 @@ const Home = (props) => {
       <div></div>
       <Footer />
     </main>
+    }
+  }
+
+
+
+  return (
+    printHome()
+    // <main
+    //   className="grid grid-cols-3 grid-rows-3 bg-cover bg-center w-full h-full m-auto bg-local place-content-center"
+    //   style={{ backgroundImage: `url(${theatreImage})` }}
+    // >
+    //   <HeaderApp />
+    //   <div></div>
+    //   <div className="row-span-3 w-full max-h-[300px] items-center justify-center mx-auto bg-red-900 rounded-md flex flex-col space-y-5">
+    //     <label className="text-slate-50 font-mono font-bold italic text-xl" for="mtitle">TITLE:</label>
+    //     <input className="rounded-md w-[300px]" onChange={handleInput}></input>
+    //     <div className="flex space-x-5"><Link to="/build/browse" state={input}><button className="bg-slate-50 rounded-lg w-20 h-10 border-2 border-solid border-black font-mono" >Search</button></Link><Link to="/build/browse"><button className="bg-slate-50 rounded-lg w-40 h-10 border-2 border-solid border-black font-mono">Show All Movies</button></Link></div>
+    //   </div>
+    //   <div></div>
+    //   <Footer />
+    // </main>
   );
 };
 
